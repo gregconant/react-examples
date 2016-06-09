4292acbcb72373e10f0179639900f4b9d80976bb
Greg Conant <g.conant@elsevier.com>
2016-06-09 06:54:18 -0400

    gc - created ListItem component to display items for dropdown. Still have to do showing/hiding based on clicked state.

--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -3,15 +3,26 @@
 // based on when user clicks on button
 var React = require('react');
 var Button = require('./button');
-//var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
     alert('hello from dropdown');
     },
   render: function() {
+    var list = this.props.items.map(function(item) {
+      return <ListItem item={item} />
+    });
     return <div class="dropdown">
-      <Button whenClicked={this.handleClick} title = { this.props.title } className="btn-default" subTitleClassName="caret" />
+      <Button
+        whenClicked={this.handleClick}
+        title = { this.props.title }
+        className="btn-default"
+        subTitleClassName="caret"
+      />    
+      <ul>
+        {list}
+      </ul>
     </div>
 }
 
