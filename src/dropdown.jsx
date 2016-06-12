09203d0511ffb7d7a222aa9733c56bf46255ca71
Greg Conant <greg.conant@gmail.com>
2016-06-12 09:00:12 -0400

    gc - finishing dropdown state example

--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -8,25 +8,30 @@ var ListItem = require('./list-item');
 module.exports = React.createClass({
   handleClick: function() {
     this.setState({ open: !this.state.open });
-    },
-  handleItemClick: function(item) {
-    this.setState( {
-      open: false
-//      itemTitle:
-    });
-    },
+  },
   getInitialState: function() {
     return { open: false }
   },
+  handleItemClick: function(item) {
+    this.setState({
+      open: false,
+      itemTitle: item
+    });
+  },
   render: function() {
     var list = this.props.items.map(function(item) {
-      return <ListItem item={item} whenItemClicked={this.handleItemClick } />
+      return <ListItem
+              item={item}
+              whenItemClicked={this.handleItemClick }
+              className={this.state.itemTitle == item ? "active" : ""} 
+              />
     }.bind(this));
+
     return <div className="dropdown">
       <Button
         whenClicked={this.handleClick}
         className="btn-default"
-        title = { this.props.title }
+        title = { this.state.itemTitle || this.props.title }
         subTitleClassName="caret"
       />
     <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
